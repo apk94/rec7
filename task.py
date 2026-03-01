f7570eaee25d22d084e397410afdee6015f6abbd
apk94 <146372877+apk94@users.noreply.github.com>
2026-03-01 15:29:00 -0500

    Refactor SQL queries for consistency in casing

--- task.py
+++ task.py
@@ -1,53 +1,4 @@
-import sqlite3 as lite
-
-import csv
-import re
-import pandas as pd
-import pandas
-from sqlalchemy import create_engine
-
-class Task(object):
-    def __init__(self, db_name, students, grades, courses, majors):
-        self.con = lite.connect(db_name)
-        self.cur = self.con.cursor()
-
-        self.cur.execute('DROP TABLE IF EXISTS Courses')
-        self.cur.execute("CREATE TABLE Courses(cid INT, number INT, professor TEXT, major TEXT, year INT, semester TEXT)")
-
-        self.cur.execute('DROP TABLE IF EXISTS Majors')
-        self.cur.execute("CREATE TABLE Majors(sid INT, major TEXT)")
-
-        self.cur.execute('DROP TABLE IF EXISTS Grades')
-        self.cur.execute("CREATE TABLE Grades(sid INT, cid INT, credits INT, grade INT)")
-
-        self.cur.execute('DROP TABLE IF EXISTS Students')
-        self.cur.execute("CREATE TABLE Students(sid INT, firstName TEXT, lastName TEXT, yearStarted INT)")
-
-        engine = create_engine("sqlite:///"+db_name)
-        df1 = pd.read_csv(students)
-        df1.to_sql('students', engine, if_exists='append', index=False)
-
-        df2 = pd.read_csv(grades)
-        df2.to_sql('grades', engine, if_exists='append', index=False)
-
-        df3 = pd.read_csv(courses)
-        df3.to_sql('courses', engine, if_exists='append', index=False)
-
-        df4 = pd.read_csv(majors)
-        df4.to_sql('majors', engine, if_exists='append', index=False)
-
-        self.cur.execute("DROP VIEW IF EXISTS allgrades")
-        self.cur.execute("""
-        create view allgrades as
-        SELECT s.firstName, s.lastName, m.major as ms, 
-               c.number, c.major as mc, g.grade 
-        FROM students as s, majors as m, grades as g, courses as c
-        WHERE s.sid = m.sid AND g.sid = s.sid AND g.cid = c.cid
-        """)
-
-
-    #q0 is an example  
-    def q0(self):
+def q0(self):
         query = '''
             SELECT * FROM students
         '''
@@ -67,17 +18,17 @@ class Task(object):
         self.cur.execute(query)
         all_rows = self.cur.fetchall()
         return all_rows
-    
+
     def q2(self):
         query = '''
-            SELECT s.firstname, s.lastname, c.year, c.semester, COUNT(*)
+            SELECT s.firstName, s.lastName, c.year, c.semester, COUNT(*)
             FROM Students s 
             JOIN Grades g ON s.sid = g.sid 
             JOIN Courses c ON g.cid = c.cid 
             WHERE g.grade > 0
             GROUP BY s.sid, c.year, c.semester
             HAVING COUNT(*) >= 2
-            ORDER BY s.firstname, s.lastname, c.year, c.semester 
+            ORDER BY s.firstName, s.lastName, c.year, c.semester 
         '''
         self.cur.execute(query)
         all_rows = self.cur.fetchall()
@@ -85,41 +36,41 @@ class Task(object):
 
     def q3(self):
         query = '''
-            SELECT firstname, lastname, ms, number
+            SELECT firstName, lastName, ms, number
             FROM allgrades 
-            WHERE grade = 0 and ms = mc 
-            ORDER BY firstname, lastname, ms, number 
+            WHERE grade = 0 AND ms = mc 
+            ORDER BY firstName, lastName, ms, number 
         '''
         self.cur.execute(query)
         all_rows = self.cur.fetchall()
         return all_rows
-        
+
     def q4(self):
         query = '''
-            SELECT s.firstname, s.lastname, m.major, c.number
+            SELECT s.firstName, s.lastName, m.major, c.number
             FROM Students s
-            JOIN Majors m ON s.sid  = m.sid 
+            JOIN Majors m ON s.sid = m.sid 
             JOIN Grades g ON s.sid = g.sid 
             JOIN Courses c ON g.cid = c.cid 
             WHERE g.grade = 0 AND m.major = c.major 
-            ORDER BY s.firstname, s.lastname, m.major, c.number 
+            ORDER BY s.firstName, s.lastName, m.major, c.number 
         '''
         self.cur.execute(query)
         all_rows = self.cur.fetchall()
         return all_rows
 
     def q5(self):
-    query = '''
-         SELECT c.professor, COUNT(*) AS success 
-         FROM Courses c 
-         JOIN Grades g ON c.cid = g.cid 
-         WHERE g.grade >= 2
-         GROUP BY c.professor 
-         ORDER BY success DESC, c.professor ASC 
-    '''
-    self.cur.execute(query)
-    all_rows = self.cur.fetchall()
-    return all_rows
+        query = '''
+            SELECT c.professor, COUNT(*) AS success 
+            FROM Courses c 
+            JOIN Grades g ON c.cid = g.cid 
+            WHERE g.grade >= 2
+            GROUP BY c.professor 
+            ORDER BY success DESC, c.professor ASC 
+        '''
+        self.cur.execute(query)
+        all_rows = self.cur.fetchall()
+        return all_rows
 
     def q6(self):
         query = '''
@@ -138,7 +89,7 @@ class Task(object):
         self.cur.execute(query)
         all_rows = self.cur.fetchall()
         return all_rows
-        
+
 if __name__ == "__main__":
     task = Task("database.db", 'students.csv', 'grades.csv', 'courses.csv', 'majors.csv')
     rows = task.q0()
@@ -162,5 +113,3 @@ if __name__ == "__main__":
     rows = task.q6()
     print(rows)
     print()
-
-
